2cc867884749bbfafe0f7f9817e060284f889569
neutronahemd <bazubairu@st.ug.edu.gh>
2023-12-15 00:01:31 +0000

    correction of words

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -7,7 +7,7 @@ CREATE TRIGGER reduce_quantity
 AFTER INSERT ON orders
 FOR EACH ROW
 BEGIN
-	UPDATE items SET quantity = quantity - NEW after adding a new order..number
+	UPDATE items SET quantity = quantity - NEW.number
 	WHERE name = NEW.item_name
 END $$
 
