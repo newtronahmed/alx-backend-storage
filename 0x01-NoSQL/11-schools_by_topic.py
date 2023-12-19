08497db421a63f9926b7a20f6b9519bbb811d620
ahmed zubairu <bazubairu@st.ug.edu.gh>
2023-12-19 15:12:10 -0800

    11 schools by topic

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python3
-"""Task 11 module"""
+'''Task 11's module.
+'''
 
 
-def school_by_topic(mongo_collection, topic):
-    """returns the list of school having a specific topic"""
-    filter_by_topic = {
+def schools_by_topic(mongo_collection, topic):
+    '''Returns the list of school having a specific topic.
+    '''
+    topic_filter = {
         'topics': {
             '$elemMatch': {
-                    '$eq': topic,
-                },
+                '$eq': topic,
+            },
         },
     }
-
-    return [doc for doc in mongo_collection.find(filter_by_topic)
+    return [doc for doc in mongo_collection.find(topic_filter)]
